42d25a1eed370ed799d6ba19821798532476335e
sdboy <jiaoyiguanggo@gmail.com>
2026-02-01 21:28:53 +0800

    format

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 #include "test.h"
 
 int main(int, char**){
-    printf("Hello, from LeetCode!\n");
-    TestTwoSum();
+  printf("Hello, from LeetCode!\n");
+  TestTwoSum();
 }
--- src/test.c
+++ src/test.c
@@ -1,16 +1,16 @@
 #include "test.h"
 
 void TestTwoSum() {
-    int nums[] = {2, 7, 11, 15};
-    int target = 9;
-    int returnSize = 0;
-    int* result = TwoSum(nums, 4, target, &returnSize);
+  int nums[] = {2, 7, 11, 15};
+  int target = 9;
+  int returnSize = 0;
+  int* result = TwoSum(nums, 4, target, &returnSize);
 
-    if (returnSize == 2 && result[0] == 0 && result[1] == 1) {
-        printf("TestTwoSum passed.\n");
-    } else {
-        printf("TestTwoSum failed.\n");
-    }
+  if (returnSize == 2 && result[0] == 0 && result[1] == 1) {
+      printf("TestTwoSum passed.\n");
+  } else {
+      printf("TestTwoSum failed.\n");
+  }
 
-    free(result);
+  free(result);
 }
